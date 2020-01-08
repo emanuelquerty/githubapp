4a81b1e775d92565436f81beaabbda10f7491faf
emanuelquerty <emanuelinacio1@hotmail.com>
2020-01-07 19:01:23 -0700

    Added the feature to display all users in home page returned from single call to api. No pagination added yet

--- js/index.js
+++ js/index.js
@@ -15,3 +15,56 @@ $(".side-navbar__close-icon").click(() => {
   $(".side-navbar__big-shadow_onVisible").hide();
   $(".side-navbar").toggleClass("slide-from-left");
 });
+
+let data = null;
+let page = 1;
+
+async function getData(page) {
+  let url = `https://api.github.com/users?page=${page}`;
+  let res = await fetch(url);
+  return await res.json();
+}
+
+getData().then(users => {
+  console.log(users);
+
+  let docFrag = document.createDocumentFragment();
+  for (let i = 0; i < users.length; i++) {
+    const { login, avatar_url } = users[i];
+    let userContainerNode = populateUserContainerNode(avatar_url, login);
+    docFrag.appendChild(userContainerNode);
+  }
+  document.querySelector(".main").appendChild(docFrag);
+});
+
+// Returns a userContainer with data passed as arguments
+function populateUserContainerNode(avatar_url, login) {
+  // Get userContainerNode from template
+  let userContainerNode = getUserNodeFromTemplate();
+
+  // Populate userContainerNode
+  let img = userContainerNode.querySelector(
+    ".avatar-and-name-container__avatar"
+  );
+  let username = userContainerNode.querySelector(
+    ".avatar-and-name-container__name"
+  );
+  img.src = avatar_url;
+  username.innerHTML = login;
+
+  return userContainerNode;
+}
+
+// Returns the content (div) inside the <template />
+function getUserNodeFromTemplate() {
+  // Get the template element
+  let template = document.querySelector("template");
+
+  // Get the div element from the template
+  let userContainerElement = template.content.querySelector("div");
+
+  // Create a new node based on the template
+  let userContainerNode = document.importNode(userContainerElement, true);
+
+  return userContainerNode;
+}
